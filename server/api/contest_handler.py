79129592378e89a5fe3645ff861df3f0e76fcdf7
Simeon Gellert <58584536+Simeon10@users.noreply.github.com>
2021-01-12 23:43:49 -0800

    Forgot to hit save, this is what I wanted to initially post

--- server/api/contest_handler.py
+++ server/api/contest_handler.py
@@ -1,24 +1,23 @@
 import json
 from flask import jsonify, Blueprint, request
 from api.models import contest_table, db
+from datetime import date
 contest_handler = Blueprint('contest_new_handler', __name__)
 
 
 @contest_handler.route('/create_new_contest', methods=['POST'])
 def create_contest():
     if request.method == 'POST':
-        body = request.json
 
-        id = body['id']
-        title = body['title']
-        description = body['description']
-        prize_contest = body['prize_contest']
-        deadline_date = body['deadline_date']
-        created_time = body['created_time']
-        update_time = body['update_time']
-        contest_creator = body['contestCreator']
+        title = request.form['title']
+        description = request.form['description']
+        prize_contest = request.form['amount']
+        deadline_date = request.form['deadline_date']
+        created_time = date.today()
+        update_time = date.today()
+        contest_creator = request.json['contestCreator']
         
-        new_contest = contest_table(id, title, description, prize_contest, deadline_date, created_time, update_time, contest_creator)
+        new_contest = contest_table(title, description, prize_contest, deadline_date, created_time, update_time, contest_creator)
         db.session.add(new_contest)
         db.session.commit()
 
@@ -26,6 +25,32 @@ def create_contest():
 
 @contest_handler.route('/contests', methods=['GET'])
 def get_all_contests():
-    if request.method == 'GET':
-        all_contests = contest_table.query.all()
-        return jsonify(all_contests)
+    all_contests = contest_table.query.all()
+    return jsonify(all_contests)
+
+@contest_handler.route('/contest/<contest_id>')
+def get_contest(contest_id):
+    contest = contest_table.query.get(contest_id)
+    return jsonify(contest)  
+
+@contest_handler.route('/update_contest/<contest_id>', methods=['PUT'])
+def update_contest(contest_id):
+    if request.method == 'PUT':
+        contest = contest_table.query.get(contest_id)
+
+        title = request.form['title']
+        description = request.form['description']
+        prize_contest = request.form['amount']
+        deadline_date = request.form['deadline_date']
+        update_time = date.today()
+
+        contest.title = title
+        contest.description = description
+        contest.prize_contest = prize_contest
+        contest.deadline_date = deadline_date
+        contest.update_time = update_time
+
+        db.session.commit()
+
+        return jsonify({'successMessage': 'Contest Updated!'})
+        
