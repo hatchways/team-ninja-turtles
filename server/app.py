11c104f4824b13cefecea19dda8169700b6c06da
Simeon Gellert <58584536+Simeon10@users.noreply.github.com>
2021-01-13 14:45:26 -0800

    AWS S3 Added

--- server/app.py
+++ server/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, Response
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
+<<<<<<< Updated upstream
+=======
+from api.contest_handler import contest_handler
+from config import S3_BUCKET, S3_KEY, S3_SECRET, S3_REGION
+import boto3
+>>>>>>> Stashed changes
 
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET,
+    region_name=S3_REGION)
 
 app = Flask(__name__)
 
--- server/config.py
+++ server/config.py
@@ -1,3 +1,8 @@
 import os
 
 TEAM_NAME = os.environ['TEAM_NAME']
+
+S3_BUCKET = os.environ.get("S3_BUCKET")
+S3_KEY = os.environ.get("S3_KEY")
+S3_SECRET = os.environ.get("S3_SECRET")
+S3_REGION = os.environ.get("S3_REGION")
