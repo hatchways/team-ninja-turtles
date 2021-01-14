11860935feee48acaa25b4a656e0e7dd621f75e8
jhanvi811 <jhanvipatel811@gmail.com>
2021-01-14 22:09:01 +0530

    models.py changes for id and created_time
    - id column  : autoincrement=True, nullable=False
    - created_time deleted from __init __ methods.

--- server/models.py
+++ server/models.py
@@ -5,22 +5,21 @@ from sqlalchemy import ForeignKey
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     username = db.Column(db.String())
     email = db.Column(db.String())
     password = db.Column(db.String())
     created_time = db.Column(db.DateTime, default=func.utcnow())
 
-    def __init__(self, id, email, password, created_time):
+    def __init__(self, id, email, password):
         self.id = id
         self.email = email
         self.password = password
-        self.created_time = created_time
 
 class Contest(db.Model):
     __tablename__ = 'contest'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     title = db.Column(db.String())
     description = db.Column(db.String())
     prize_contest = db.Column(db.Integer())
@@ -29,20 +28,19 @@ class Contest(db.Model):
     update_time = db.Column(db.DateTime, onupdate=func.utcnow()) # store updated time of row
     contest_creater = db.Column(db.Integer, ForeignKey('user.id'))
 
-    def __init__(self, id, title, description, prize_contest, deadline_date, created_time, update_time, contest_creater):
+    def __init__(self, id, title, description, prize_contest, deadline_date, update_time, contest_creater):
         self.id = id
         self.title = title
         self.description = description
         self.prize_contest = prize_contest
         self.deadline_date = deadline_date
-        self.created_time = created_time
         self.update_time = update_time
         self.contest_creater = contest_creater
 
 class Submission(db.Model):
     __tablename__ = 'submission'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     contest_id = db.Column(db.Integer, ForeignKey('contest.id'))
     submiter_id = db.Column(db.Integer, ForeignKey('user.id'))
     active = db.Column(db.Boolean, default=False, nullable=False)
@@ -50,11 +48,10 @@ class Submission(db.Model):
     created_time = db.Column(db.DateTime, default=func.utcnow())
     update_time = db.Column(db.DateTime, onupdate=func.utcnow()) # updated time of row
 
-    def __init__(self, id, contest_id, submiter_id, active, image_link, created_time, update_time):
+    def __init__(self, id, contest_id, submiter_id, active, image_link, update_time):
         self.id = id
         self.contest_id = contest_id
         self.submiter_id = submiter_id
         self.active = active
         self.image_link = image_link
-        self.created_time = created_time
         self.update_time = update_time
