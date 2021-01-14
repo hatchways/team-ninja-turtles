9359d086e4729557a265ce9965d538e8f2fc54d4
Simeon Gellert <58584536+Simeon10@users.noreply.github.com>
2021-01-14 09:53:26 -0800

    Updated AWS and Contest Handler

--- server/app.py
+++ server/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-import os
+from flask import Flask, Response
 from api.ping_handler import ping_handler
-
 from api import db, bcrypt
-
 from api.home_handler import home_handler
+from api.contest_handler import contest_handler
 from api.user_handler import user_handler
+from config import S3_BUCKET, S3_KEY, S3_SECRET, S3_REGION
+import os
+import boto3
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET,
+    region_name=S3_REGION)
 
 app = Flask(__name__)
 
@@ -27,5 +34,4 @@ bcrypt.init_app(app)
 app.register_blueprint(user_handler)
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-
-
+app.register_blueprint(contest_handler)
