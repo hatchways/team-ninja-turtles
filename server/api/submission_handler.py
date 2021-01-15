f8acc8f3ce80fb3a028515c43b8b49e15fa471b1
jhanvi811 <jhanvipatel811@gmail.com>
2021-01-15 21:11:26 +0530

    changes

--- server/api/submission_handler.py
+++ server/api/submission_handler.py
@@ -6,7 +6,7 @@ submission_handler = Blueprint('submission_new_handler', __name__)
 
 
 @submission_handler.route('/contestImage/submission/<contest_id>', methods=['POST'])
-def create_submission(contest_id, user_id):
+def create_submission(contest_id):
     try:
         contest = Contest.query.filter_by(id=contest_id)
         if not contest:
@@ -17,7 +17,7 @@ def create_submission(contest_id, user_id):
 
     if request.method == 'POST':
         data = request.form
-        new_submission = Submission(contest_id=contest.id, submiter_id=data.get['user_id'], active=data.get('status'),
+        new_submission = Submission(contest_id=contest.id, submiter_id=data.get('user_id'), active=data.get('status'),
                                     image_link=data.get('link'), update_time=date.today())
         db.session.add(new_submission)
         db.session.commit()
