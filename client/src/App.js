656bdd63ad8396e4daf2598eab900944df15d53a
Edward Tan <edyhtan@outlook.com>
2021-01-15 13:17:25 -0500

    actually resolving the conflict...as VScode did not reflect the changes.

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,12 @@ import React from 'react'
 import { MuiThemeProvider } from '@material-ui/core'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-<<<<<<< HEAD
 import { theme } from "./themes/theme"
 import NavBar from "./components/Navbar"
 import LandingPage from "./pages/Landing"
 import CreateContestPage from "./pages/CreateContest"
 import Login from "./pages/Login"
-=======
-import { theme } from './themes/theme'
-import NavBar from './components/Navbar'
->>>>>>> origin/dev
+
 
 import SignupPage from './pages/Signup'
 import LoginPage from './pages/Login'
@@ -27,17 +23,11 @@ function App() {
       <Router>
         <NavBar />
         <Switch>
-<<<<<<< HEAD
-          <Route path="/create-contest" component={CreateContestPage}></Route>
-          <Route path="/" component={LandingPage} />
-          <Route path="/login" component={Login}></Route>
-=======
           <Route path='/signup' component={SignupPage}></Route>
           <Route path='/login' component={LoginPage}></Route>
           <Route path='/create-contest' component={CreateContestPage}></Route>
           <Route path='/submit-design' component={SubmitDesignPage}></Route>
           <Route path='/' component={LandingPage} />
->>>>>>> origin/dev
         </Switch>
       </Router>
     </MuiThemeProvider>
