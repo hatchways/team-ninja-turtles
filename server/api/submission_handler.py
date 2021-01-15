a923dabcc303fed9d1098b468a6d3e092091b674
jhanvi811 <jhanvipatel811@gmail.com>
2021-01-15 08:48:40 +0530

    @bonnieli suggestion and changes applied.

--- server/api/submission_handler.py
+++ server/api/submission_handler.py
@@ -5,7 +5,7 @@ from datetime import date
 submission_handler = Blueprint('submission_new_handler', __name__)
 
 
-@submission_handler.route('/contestImage/submission/<contest_id>/<user_id>', methods=['POST'])
+@submission_handler.route('/contestImage/submission/<contest_id>', methods=['POST'])
 def create_submission(contest_id, user_id):
     try:
         contest = Contest.query.filter_by(id=contest_id)
@@ -18,18 +18,9 @@ def create_submission(contest_id, user_id):
 
     if request.method == 'POST':
         data = request.form
-        new_submission = Submission(contest_id=contest.id, submiter_id=submission_user.id, active=data.get('status'),
+        new_submission = Submission(contest_id=contest.id, submiter_id=request.json['user_id'], active=data.get('status'),
                                     image_link=data.get('link'), update_time=date.today())
         db.session.add(new_submission)
         db.session.commit()
 
         return jsonify({'successMessage': 'New submission created successfully'})
-
-
-
-
-
-
-
-
-
