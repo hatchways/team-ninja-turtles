cc1cf4ebea18e7de72988e21121db20b0b12ac27
Edward Tan <edyhtan@outlook.com>
2021-01-15 08:58:42 -0500

    Adding js-cookies

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { theme } from "./themes/theme"
 import NavBar from "./components/Navbar"
 import LandingPage from "./pages/Landing"
 import CreateContestPage from "./pages/CreateContest"
-
+import Login from "./pages/Login"
 import "./App.css"
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/create-contest" component={CreateContestPage}></Route>
+          <Route path="/login" component={Login}></Route>
           <Route path="/" component={LandingPage} />
         </Switch>
       </Router>
