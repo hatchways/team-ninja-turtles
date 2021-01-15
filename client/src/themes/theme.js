7b13742596aedf947f10b70520216a5dfcab4495
banh0006 <banh0006@algonquinlive.com>
2021-01-14 19:16:01 -0500

    fixed font name mistake

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -2,7 +2,7 @@ import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: '"c", Roboto"'
+    fontFamily: '"Poppins", Roboto"'
   },
   primary: "#f04040",
   secondary: "#1f1f1f",
