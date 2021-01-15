a552ce47567dbff822caa1490586f57d48a13cc3
banh0006 <banh0006@algonquinlive.com>
2021-01-14 20:37:29 -0500

    added drag and drop feature

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { AppBar, Typography, Button, Toolbar, Avatar } from '@material-ui/core'
+import { AppBar, Typography, Button, Toolbar } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
--- client/src/pages/SubmitDesign.js
+++ client/src/pages/SubmitDesign.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Paper, Typography, Button, Box } from '@material-ui/core'
+import { useDropzone } from 'react-dropzone'
 
 const useStyles = makeStyles((theme) => ({
     pageContainer: {
@@ -60,28 +61,57 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SubmitDesign() {
     const classes = useStyles()
+    const {
+        acceptedFiles,
+        fileRejections,
+        getRootProps,
+        getInputProps
+    } = useDropzone({
+        accept: 'image/jpeg, image/png, img/gif',
+        maxFiles: 1
+    })
+
+    const acceptedFileItems = acceptedFiles.map((file, index)  => (
+        <div key={index}>
+            <h4>Upload success</h4>
+            <li key={file.path}>
+                {file.path} - {file.size} bytes
+            </li>
+        </div>
+    ))
+    
+    const fileRejectionItems = fileRejections.map(({ file, errors }, index) => (
+        <div key={index}>
+            <h4>Upload failed</h4>
+            Fail to upload {file.path} - {file.size} bytes
+            <ul>
+                {errors.map(e => (
+                    <li key={e.code}>{e.message}</li>
+                ))}
+            </ul>
+        </div>
+    ))
 
     return (
         <div className={classes.pageContainer}>
-            <Paper className={classes.paper}>
-                <Typography className={classes.pageTitle}>Submit Design</Typography>
-                <input
-                    accept='image/*'
-                    style={{ display: 'none' }}
-                    id='choose-file-button'
-                    type='file'
-                />
-                <label htmlFor='choose-file-button'>
+            <div {...getRootProps({ className: 'dropzone' })}>
+                <input {...getInputProps()} />
+                <Paper className={classes.paper}>
+                    <Typography className={classes.pageTitle}>Submit Design</Typography>
                     <Button component='span' className={classes.uploadButton}>
                         <img src={process.env.PUBLIC_URL + '/images/upload-icon.png'} alt='Upload Icon' className={classes.image} />
                     </Button>
-                </label> 
-                <div className={classes.instructionDiv}>
-                    <Typography className={classes.boldText}>Click to choose a file</Typography>
-                    <Typography>High resolution images</Typography>
-                    <Typography>PNG, JPG, GIF</Typography>
-                </div>
-            </Paper>
+                    <div className={classes.instructionDiv}>
+                        <Typography className={classes.boldText}>Click to choose a file</Typography>
+                        <Typography>High resolution images</Typography>
+                        <Typography>PNG, JPG, GIF</Typography>
+                    </div>
+                </Paper>
+                <aside>
+                    {acceptedFiles.length > 0 ? acceptedFileItems : (<div></div>)}
+                    {fileRejectionItems.length > 0 ? fileRejectionItems : (<div></div>)}
+                </aside>
+            </div>
             <Box className={classes.buttonWrapper}>
                 <Button className={classes.submitButton}>Submit</Button>
             </Box>
