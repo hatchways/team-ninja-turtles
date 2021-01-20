5ae0e080d3f69dc85fbf01c77e3c03b2518dde4d
jhanvi811 <jhanvipatel811@gmail.com>
2021-01-20 23:09:46 +0530

    Backend changes

--- server/api/socketio_handler.py
+++ server/api/socketio_handler.py
@@ -1,11 +1,13 @@
 from flask import jsonify, Blueprint, request
 from api import socketio
-from flask_socketio import emit
+from flask_socketio import send, emit
 from datetime import datetime
+
 socketio_handler = Blueprint('socketio_new_handler', __name__)
 
 
-@socketio.on('connect')
-def connect_socket():
-    emit('connect', {'data': datetime.now().isoformat()})
+@socketio.on("message")
+def handleMessage(msg):
+    print(msg)
+    send(msg, broadcast=True)
     return None
