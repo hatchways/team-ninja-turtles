c5c24dcffb9867c5a06bcf4b9538c35a081d17dd
Edward Tan <edyhtan@outlook.com>
2021-01-29 14:45:29 -0500

    merge DB migration heads.

---
+++ server/migrations/versions/63cbba031139_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 63cbba031139
+Revises: b3f7ad8646bd, 2d89846aebea
+Create Date: 2021-01-29 14:44:37.832836
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63cbba031139'
+down_revision = ('b3f7ad8646bd', '2d89846aebea')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
