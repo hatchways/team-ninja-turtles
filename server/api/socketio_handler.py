f3fd5f9d831575fef306b138dc8a81017e77e202
Edward Tan <edyhtan@outlook.com>
2021-01-28 22:21:29 -0500

    adding flask-socket.io, removing unused data model.

--- server/api/socketio_handler.py
+++ server/api/socketio_handler.py
@@ -1,13 +1,12 @@
 from flask import jsonify, Blueprint, request
 from api import socketio, db
-from .models import Messages
 from flask_socketio import send, emit, join_room
 
 socketio_handler = Blueprint('socketio_new_handler', __name__)
 
 
 @socketio.on("message")
-def handleMessage(msg):
+def handle_message(msg):
     send({"message": msg["message"], "name": msg["username"]}, broadcast=True, room=msg["room_id"])
     return None
 
