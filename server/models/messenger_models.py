b0c11401c9175e77db56b5c5ca5c5c98ece4beb2
Edward Tan <edyhtan@outlook.com>
2021-02-02 11:56:11 -0500

    re-modeling user-session relationship.

--- server/models/messenger_models.py
+++ server/models/messenger_models.py
@@ -3,13 +3,19 @@ from sqlalchemy import ForeignKey
 from datetime import datetime
 
 
+user_in_session = db.Table(
+    'room_session_user',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('room_id', db.Integer, db.ForeignKey('room_session.id'))
+)
+
+
 class RoomSession(db.Model):
     __tablename__ = "room_session"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    user1 = db.Column(db.Integer, ForeignKey("user.id"))
-    user2 = db.Column(db.Integer, ForeignKey("user.id"))
-    messages = db.relationship('message', backref="id")
+    messages = db.relationship('Message', primaryjoin="RoomSession.id == Message.session", backref="room")
+    users = db.relationship('User', secondary=user_in_session, backref=db.backref("sessions"), lazy="dynamic")
 
 
 class Message(db.Model):
